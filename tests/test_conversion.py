fd585bcfacb003bed9440a301eb59678d84e3ad4
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-08-09 16:00:07 -0400

    black .

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -2,7 +2,7 @@ import o2conversion
 import pytest
 
 
-@pytest.mark.parametrize("O2conc,T,S,P,expected_value", [(2, 2, 2,2, 0.98904)])
+@pytest.mark.parametrize("O2conc,T,S,P,expected_value", [(2, 2, 2, 2, 0.98904)])
 def test_O2ctoO2p_conversion(O2conc, T, S, P, expected_value):
     value = o2conversion.O2ctoO2p(O2conc, T, S, P)
     assert value == pytest.approx(expected_value, 1e-5)
@@ -22,7 +22,9 @@ def test_O2ptoO2c_conversion(pO2, T, S, P, expected_value):
     assert value == pytest.approx(expected_value, 1e-5)
 
 
-@pytest.mark.parametrize("pO2,T,S,P,p_atm,expected_value", [(2, 2, 2, 2,1000, 0.96161)])
+@pytest.mark.parametrize(
+    "pO2,T,S,P,p_atm,expected_value", [(2, 2, 2, 2, 1000, 0.96161)]
+)
 def test_O2ptoO2s_conversion(pO2, T, S, P, p_atm, expected_value):
     value = o2conversion.O2ptoO2s(pO2, T, S, P, p_atm)
     assert value == pytest.approx(expected_value, 1e-5)
